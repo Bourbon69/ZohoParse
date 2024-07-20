c09fd2a0bb397434397e602dec0d61351cb3e749
Bourbon69 <britannia.bb.bourbon@gmail.com>
2024-07-20 12:05:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/:mobile',  async(req, res) => {
     const {mobile} = req.params
 
     const {data} = await axios.get("https://www.zohoapis.in/crm/v2/functions/get_users_recipe_list/actions/execute?auth_type=apikey&zapikey=1003.15ad2c644dc062923f46f9ff3424587f.355c7a13098ec1b31c265153385f3941&mobile="+mobile)
-    res.status(200).send(data.details.output);
+    res.status(200).json({"output": data.details.output});
    } catch (error) {
     console.log(error)
    }
