c28e0dedc4ca2bd6c2ba5ab01770ab5e44fd4ca3
Bourbon69 <britannia.bb.bourbon@gmail.com>
2024-07-21 01:19:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.get('/:mobile',  async(req, res) => {
    }
 })
 
+app.post('/sendrecipe',  async(req, res) => {
+   try {
+    const {data} = req.body
+    res.status(200).json({"output": data });
+   } catch (error) {
+    console.log(error)
+   }
+})
+
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
